1bb53b19f3b8bc8b50b0073170b95548ac632d72
adriamPaz <a24adriamps@iessanclemente.net>
2026-02-04 16:36:27 +0100

    preparacion examen

---
+++ src/main/java/com/xulioxesus/Pedido.java
@@ -0,0 +1,24 @@
+package com.xulioxesus;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pedido {
+    private List<Producto> productos;
+
+    public Pedido() {
+        productos = new ArrayList<>();
+    }
+
+    public void agregarProducto(Producto producto) {
+        productos.add(producto);
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for (Producto producto : productos) {
+            total += producto.getTotal();
+        }
+        return total;
+    }
+}
